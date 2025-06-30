355dc9f4f41e6b98eb505d8a89a2b50a24297137
chltmdgh522 <chltmdgh517@naver.com>
2025-06-30 22:37:44 +0900

    Feat: 알레르기 등록(수정) 및 조회 API에 Swagger 문서 적용

--- src/main/java/babbuddy/domain/allergy/presentation/dto/res/AllergyGetRes.java
+++ src/main/java/babbuddy/domain/allergy/presentation/dto/res/AllergyGetRes.java
@@ -2,10 +2,16 @@ package babbuddy.domain.allergy.presentation.dto.res;
 
 
 import babbuddy.domain.allergy.domain.entity.AllergyType;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "알레르기 조회 응답")
 public record AllergyGetRes(
-        String allergyType, // enum name
-        String koreanName   // 한글 표시용
+        @Schema(description = "알레르기 타입 (영문 enum 이름)", example = "EGG")
+        String allergyType,
+
+        @Schema(description = "알레르기 한글명", example = "달걀")
+        String koreanName
+
 ) {
     public static AllergyGetRes of(AllergyType allergyType) {
         return new AllergyGetRes(
