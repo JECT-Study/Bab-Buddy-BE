fb2a20bf26f7b60534fd4e9822ecbca2c3f4e4ca
chltmdgh522 <chltmdgh517@naver.com>
2025-07-13 14:48:12 +0900

    Feat: 닉네임 수정 API 개발 완료

--- src/main/java/babbuddy/domain/user/application/service/UserService.java
+++ src/main/java/babbuddy/domain/user/application/service/UserService.java
@@ -1,8 +1,10 @@
 package babbuddy.domain.user.application.service;
 
 
+import babbuddy.domain.user.presentation.dto.req.NicknameReq;
+
 public interface UserService {
 
-
+    void updateNickname(String userId, NicknameReq req);
 
 }
--- src/main/java/babbuddy/domain/user/presentation/controller/UserController.java
+++ src/main/java/babbuddy/domain/user/presentation/controller/UserController.java
@@ -1,18 +1,39 @@
 package babbuddy.domain.user.presentation.controller;
 
+import babbuddy.domain.dislikefood.presentation.dto.res.GetDisLikeRes;
 import babbuddy.domain.user.application.service.UserService;
+import babbuddy.domain.user.presentation.dto.req.NicknameReq;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/user")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "User", description = "닉네임 수정 API")
 public class UserController {
 
+    private final UserService userService;
 
+    @Operation(summary = "닉넴 수정", description = "사용자 닉네임을 수정합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "닉네임 수정 성공"),
+            @ApiResponse(responseCode = "400", description = "유저 존재하지 않음")
+    })
+    @PatchMapping
+    public void updateNickname(@RequestBody @Valid NicknameReq req, @AuthenticationPrincipal String userId) {
+        userService.updateNickname(userId,req);
+    }
 
 }
