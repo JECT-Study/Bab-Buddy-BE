3267f3041d2b24df06cdb761a7fd11b0a58320b3
chltmdgh522 <chltmdgh517@naver.com>
2025-10-02 23:29:37 +0900

    Fix: 로그 개선

--- src/main/java/babbuddy/domain/oauth2/presentation/controller/LoginLinkController.java
+++ src/main/java/babbuddy/domain/oauth2/presentation/controller/LoginLinkController.java
@@ -3,6 +3,7 @@ package babbuddy.domain.oauth2.presentation.controller;
 import babbuddy.domain.oauth2.application.service.LoginLinkService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/oauth2")
 @RequiredArgsConstructor
+@Slf4j
 public class LoginLinkController {
 
     private final LoginLinkService loginLinkService;
@@ -20,7 +22,9 @@ public class LoginLinkController {
     public ResponseEntity<Void> loginPage(HttpServletRequest request) {
         // 로그인 링크 생성 시 state를 포함
         String loginLink = loginLinkService.getLoginLink(request);
+        String origin = request.getParameter("origin");
 
+        log.info(origin);
         return ResponseEntity
                 .status(HttpStatus.FOUND)   // 302 Redirect
                 .header("Location", loginLink)
