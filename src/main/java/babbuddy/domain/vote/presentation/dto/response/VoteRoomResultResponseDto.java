d68c483e2dc4edea63689e67b60065a385edf2d5
king_0417 <73704053+gywns0417@users.noreply.github.com>
2025-10-17 19:09:03 +0900

    그룹방 상세 조회, 결과 조회 시 menuSelectMethod 필드 값 반환 구현 (#39)
    * feat: 그룹방 상세조회 시 menuSelectMethod 필드 값 반환
    
    * feat: 그룹방 결과 조회 시 menuSelectMethod 필드 값 반환

--- src/main/java/babbuddy/domain/vote/presentation/dto/response/VoteRoomResultResponseDto.java
+++ src/main/java/babbuddy/domain/vote/presentation/dto/response/VoteRoomResultResponseDto.java
@@ -1,5 +1,6 @@
 package babbuddy.domain.vote.presentation.dto.response;
 
+import babbuddy.domain.voteroom.domain.entity.MenuSelectMethod;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,4 +12,5 @@ public class VoteRoomResultResponseDto {
     private String voteRoomId;
     private String title;
     private VoteResultDto result;
+    private MenuSelectMethod menuSelectMethod;
 }
