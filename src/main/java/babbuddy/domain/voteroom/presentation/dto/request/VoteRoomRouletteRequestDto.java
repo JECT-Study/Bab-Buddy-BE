0062b9bc396ba9b0edb288bff4f260467e6b3513
king_0417 <73704053+gywns0417@users.noreply.github.com>
2025-10-17 20:47:22 +0900

    feat: 룰렛 메뉴 등록 기능 추가 및 관련 DTO, 에러 코드 수정

---
+++ src/main/java/babbuddy/domain/voteroom/presentation/dto/request/VoteRoomRouletteRequestDto.java
@@ -0,0 +1,17 @@
+package babbuddy.domain.voteroom.presentation.dto.request;
+
+import babbuddy.domain.voteroom.domain.entity.MenuSelectMethod;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class VoteRoomRouletteRequestDto {
+
+    @NotNull(message = "메뉴 선택 방법은 필수입니다.")
+    private MenuSelectMethod menuSelectMethod;
+
+    @NotNull(message = "메뉴 아이디는 필수입니다.")
+    private String menuId;
+}
